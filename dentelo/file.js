2ea37245de804a05c37e9efeaeee51a7127c3ebf
Shravan <72138270+Shravan773@users.noreply.github.com>
2024-06-11 14:27:06 +0530

    Add files via upload

---
+++ dentelo/file.js
@@ -0,0 +1,16 @@
+const options = {
+  bottom: '64px',
+  right: 'unset',
+  left: '32px',
+  time: '0.5s',
+  mixColor: '#fff',
+  backgroundColor: '#fff',
+  buttonColorDark: '#100f2c',
+  buttonColorLight: '#fff',
+  saveInCookies: false,
+  label: '🌓',
+  autoMatchOsTheme: true
+};
+
+  const darkmode = new Darkmode(options);
+darkmode.showWidget();
---
+++ dentelo/script.js
@@ -0,0 +1,79 @@
+
+const options = {
+  bottom: '64px', 
+  right: 'unset',
+  left: '32px', 
+  time: '0.5s', 
+  mixColor: '#fff', 
+  backgroundColor: '#fff',  
+  buttonColorDark: '#100f2c',  
+  buttonColorLight: '#fff', 
+  saveInCookies: false, 
+  label: '🌓', 
+  autoMatchOsTheme: true 
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+const openFormBtn = document.getElementById('open-form-btn');
+
+openFormBtn.addEventListener('click', function() {
+	window.open('book.html', 'User Data Form', 'width=600,height=500');
+});
+
+
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  navbarToggler.classList.toggle("active");
+}
+
+addEventOnElem(navbarToggler, "click", toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  navbarToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNav);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
